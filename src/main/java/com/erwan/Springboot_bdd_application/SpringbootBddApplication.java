cdb9ec101d5d252e53e89131d184024e63a1abb3
MsNCreatureS <156552946+MsNCreatureS@users.noreply.github.com>
2026-03-11 15:10:13 +0100

    Initial commit

---
+++ src/main/java/com/erwan/Springboot_bdd_application/SpringbootBddApplication.java
@@ -0,0 +1,13 @@
+package com.erwan.Springboot_bdd_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erwan/Springboot_bdd_application/SpringbootBddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erwan.Springboot_bdd_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
