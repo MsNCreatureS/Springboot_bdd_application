1834d6169bb2e76cbe0464f288af7f330704d4e8
MsNCreatureS <156552946+MsNCreatureS@users.noreply.github.com>
2026-03-13 20:42:52 +0100

    Creation de la class Skills

---
+++ src/main/java/com/erwan/Springboot_bdd_application/Models/Skill.java
@@ -0,0 +1,61 @@
+package com.erwan.Springboot_bdd_application.Models;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "skills")
+
+public class Skill {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	@ManyToMany(mappedBy = "skills")
+	private List<Person> persons;
+	
+	// LE CONSTRUCTEUR VIDE (Indispensable pour Hibernate)
+    public Skill() {
+    }
+	
+	public Skill(String name) {
+	    this.name = name;
+	}
+	
+	//getter et setter
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Person> getPersons() {
+		return persons;
+	}
+
+	public void setPersons(List<Person> persons) {
+		this.persons = persons;
+	}
+	
+
+}
